55fe12a6d5418a567e6da5ec067201297ed67746
Konstantin Samarin <konstsamari@gmail.com>
2020-08-29 20:45:03 +0700

    wiki: complete specs

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path('wiki/<str:title>', views.entry, name='entry'),
+    path('edit/<str:title>/', views.edit, name='edit'),
+    path('search', views.search, name='search'),
+    path('new', views.new, name='new'),
+    path('random', views.random, name='random')
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,80 @@
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import render, redirect
+from random import choice
+from markdown2 import Markdown
 
 from . import util
 
+markdowner = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
+def entry(request, title):
+    entry = util.get_entry(title)
+
+    if entry:
+        return render(request, 'encyclopedia/entry.html', {
+            'title': title,
+            'content': markdowner.convert(entry)
+        })
+    else:
+        return render(request, 'encyclopedia/error.html')
+
+
+def search(request):
+    query = request.GET['q']
+    entry = util.get_entry(query)
+
+    if entry:
+        return redirect('entry', title=query)
+
+    entries = util.list_entries()
+    matches = []
+
+    for ent in entries:
+        if query.lower() in ent.lower():
+            matches.append(ent)
+
+    if not matches:
+        return render(request, 'encyclopedia/error.html', {
+            'message': 'Page doesn\'t exist'
+        })
+    else:
+        return render(request, 'encyclopedia/search.html', {
+            'matches': matches
+        })
+
+
+def new(request):
+    if request.method == 'POST':
+        entries = util.list_entries()
+        title = request.POST['title']
+        content = request.POST['content']
+
+        if title in entries:
+            return render(request, 'encyclopedia/error.html', {
+                'message': 'Page already exists'
+            })
+        else:
+            util.save_entry(title, content)
+            return redirect('entry', title=title)
+    else:
+        return render(request, 'encyclopedia/new.html')
+
+def edit(request, title):
+    if request.method == 'POST':
+        new_content = request.POST.get('new_content')
+        util.save_entry(title, new_content)
+        return redirect('entry', title=title)
+    else:
+        return render(request, 'encyclopedia/edit.html', {
+            'title': title,
+            'content': util.get_entry(title)
+        })
+
+def random(request):
+    return redirect('entry', title=choice(util.list_entries()))
